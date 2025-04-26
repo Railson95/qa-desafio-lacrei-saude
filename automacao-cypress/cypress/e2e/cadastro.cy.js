3050adff5d61aa2712c56bac1a7d686e68db374e
Railson Martins <railsonmartins1970@gmail.com>
2025-04-26 09:36:59 -0300

    Add tests to Cadastro

--- automacao-cypress/cypress/e2e/cadastro.cy.js
+++ automacao-cypress/cypress/e2e/cadastro.cy.js
@@ -1,82 +1,139 @@
+import { faker } from '@faker-js/faker';
+
+
 describe('Cypress Playground', () => {
   beforeEach(() => {
-    cy.visit('https://cypress-playground.s3.eu-central-1.amazonaws.com/index.html')
+    cy.visit('https://paciente-staging.lacreisaude.com.br/cadastro/')
   }) 
+  
+  it('Inserir dados válidos em todos os campos do formulário e enviar', () => {
+    let email = faker.internet.email();
+    cy.get('#first_name').type('Railson');
+    cy.get('#last_name').type('Martins da Mata');
+    cy.get('#email').type(email);
+    cy.get('#email2').type(email);
+    cy.get('#password1').type('Martins(01)'); // TODO: Esconder essa senha por segurança
+    cy.get('#password2').type('Martins(01)'); // TODO: Esconder essa senha por segurança
+    cy.get('input[name="accepted_privacy_document"]').check({ force: true });
+    cy.get('input[name="is_18_years_old_or_more"]').check({ force: true });
 
-  it('shows a promotional banner', () => {
-    cy.get('#promotional-banner')
-      .should('be.visible')
+    cy.contains('button', 'Cadastrar').click();
+
+    cy.contains('button', 'Voltar ao login').should('be.visible');
   })
 
-  it('shows what happens when click the subscribe button', () => {
-    cy.contains('button', 'Subscribe')
-      .should('be.visible')
-      .click()
+  it('Tentar enviar o formulário com todos os campos em branco', () => {
+    cy.get('#first_name').clear();
+    cy.get('#last_name').clear();
+    cy.get('#email').clear();
+    cy.get('#email2').clear();
+    cy.get('#password1').clear(); // TODO: Esconder essa senha por segurança
+    cy.get('#password2').clear(); // TODO: Esconder essa senha por segurança
+    cy.get('input[name="accepted_privacy_document"]').uncheck({ force: true });
+    cy.get('input[name="is_18_years_old_or_more"]').uncheck({ force: true });
 
-    cy.contains(
-      '#success', 
-      "You've been successfully subscribed to our newsletter."
-      ).should('be.visible')
+
+    cy.contains('button', 'Cadastrar').click();
+
+    cy.contains('p', 'O nome é obrigatório.').should('be.visible');
+    cy.contains('p', 'O sobrenome é obrigatório.').should('be.visible');
+    cy.contains('p', 'O e-mail é obrigatório.').should('be.visible');
+    cy.contains('p', 'A senha é obrigatória.').should('be.visible');
+    cy.contains('p', 'A confirmação da senha é obrigatória.').should('be.visible');
+    cy.contains('p', 'Você deve aceitar os termos.').should('be.visible');
+    cy.contains('p', 'Você deve ter 18 anos ou mais.').should('be.visible');
   })
 
-  it('inserts the signature in a input and asserts it is signed',() => {
-    cy.get('#signature-textarea').type('Railson')
+  it('Inserir dados inválidos nos campos do formulário e enviar', () => {
+    let senhaTeste1 = {
+      number: '1',
+      errorMessage: 'A senha deve ter no mínimo 8 caracteres'
+    }
+  
+    let senhaTeste2 = {
+      number: '12345678',
+      errorMessage: 'A senha deve conter pelo menos uma letra maiúscula'
+    }
+  
+    let senhaTeste3 = {
+      number: '12345678A',
+      errorMessage: 'A senha deve conter pelo menos uma letra minúscula'
+    }
+  
+    let senhaTeste4 = {
+      number: '12345678Aa',
+      errorMessage: 'A senha deve conter pelo menos um caractere especial (#!*-_&)'
+    }
+  
+    let senhaTeste5 = {
+      number: 'abcdefghA',
+      errorMessage: 'A senha deve conter pelo menos um número'
+    }
+  
+    let massaDeTesteSenhas = [senhaTeste1, senhaTeste2, senhaTeste3, senhaTeste4, senhaTeste5]
 
-    cy.contains('#signature', 'Railson').should('be.visible')
+
+    massaDeTesteSenhas.forEach((senha, index) => {
+      cy.get('#first_name').clear();
+      cy.get('#last_name').clear();
+      cy.get('#email').clear();
+      cy.get('#email2').clear();
+      cy.get('#password1').clear(); // TODO: Esconder essa senha por segurança
+      cy.get('#password2').clear(); // TODO: Esconder essa senha por segurança
+
+
+      cy.get('#first_name').type('123test');
+      cy.get('#last_name').type('125test');
+      cy.get('#email').type('123@123');
+      cy.get('#email2').type('123@123');
+      cy.get('#password1').type(senha.number); // TODO: Esconder essa senha por segurança
+      cy.get('#password2').type('151'); // TODO: Esconder essa senha por segurança
+      cy.get('input[name="accepted_privacy_document"]').uncheck({ force: true });
+      cy.get('input[name="is_18_years_old_or_more"]').uncheck({ force: true });
+
+      cy.contains('button', 'Cadastrar').click();
+
+      cy.contains('p', 'Digite um nome válido').should('be.visible');
+      cy.contains('p', 'Digite um sobrenome válido').should('be.visible');
+      cy.contains('p', 'Insira um e-mail válido').should('be.visible');
+      cy.contains('p', senha.errorMessage).should('be.visible');
+      // cy.contains('p', '').should('not.exist');
+      cy.contains('p', 'Você deve aceitar os termos.').should('be.visible');
+      cy.contains('p', 'Você deve ter 18 anos ou mais.').should('be.visible');
+    });
   })
 
-  it('inserts the signature in a input and select a checkbox and assert it is signed, then uncheck it ',() => {
-    cy.get('#signature-textarea-with-checkbox').type('Railson')
-    cy.get('#signature-checkbox').check()
-    cy.contains('#signature-triggered-by-check', 'Railson').should('be.visible')
-    cy.get('#signature-checkbox').uncheck()
-    cy.contains('#signature-triggered-by-check', 'Railson').should('not.exist')
+  it('Inserir e-mails válidos diferentes nos campos de e-mail e confirmar e-mail e enviar o formulário', () => {
+    let email1 = faker.internet.email();
+    let email2 = faker.internet.email();
+    cy.get('#first_name').type('Railson');
+    cy.get('#last_name').type('Martins da Mata');
+    cy.get('#email').type(email1);
+    cy.get('#email2').type(email2);
+    cy.get('#password1').type('Martins(01)'); // TODO: Esconder essa senha por segurança
+    cy.get('#password2').type('Martins(01)'); // TODO: Esconder essa senha por segurança
+    cy.get('input[name="accepted_privacy_document"]').check({ force: true });
+    cy.get('input[name="is_18_years_old_or_more"]').check({ force: true });
+
+    cy.contains('button', 'Cadastrar').click();
+
+    cy.contains('p', 'Os e-mails não correspondem, digite novamente.').should('be.visible');
+  })
+
+  it('Inserir senhas válidas diferentes nos campos de senha e confirmar senha e enviar o formulário', () => {
+    let email1 = faker.internet.email();
+    cy.get('#first_name').type('Railson');
+    cy.get('#last_name').type('Martins da Mata');
+    cy.get('#email').type(email1);
+    cy.get('#email2').type(email1);
+    cy.get('#password1').type('Martins(01)'); // TODO: Esconder essa senha por segurança
+    cy.get('#password2').type('Martins(02)'); // TODO: Esconder essa senha por segurança
+    cy.get('input[name="accepted_privacy_document"]').check({ force: true });
+    cy.get('input[name="is_18_years_old_or_more"]').check({ force: true });
+
+    cy.contains('button', 'Cadastrar').click();
+
+    cy.contains('p', 'Senhas incompatíveis, tente novamente.').should('be.visible');
   })
   
-  it('Turn the radio button on/off and check if the correct text appears', () => {
-    cy.contains('#on-off', 'ON').should('be.visible')
-
-    cy.get('#off').check()
-
-    cy.contains('#on-off', 'OFF').should('be.visible')
-    cy.contains('#on-off', 'ON').should('not.exist')
-
-    cy.get('#on').check()
-
-    cy.contains('#on-off', 'ON').should('be.visible')
-    cy.contains('#on-off', 'OFF').should('not.exist')
-
-  })
-
-  it('selects a field in dropdown and checks if the correct type is shown', () => {
-    cy.contains('#selection-type').should('not.exist')
-    cy.contains('#select-selection > strong').should('not.exist')
-
-    cy.get('#selection-type').select('Basic')
-    cy.contains('#select-selection > strong', 'BASIC').should('be.visible')
-  })
-
-  it('select multiple options and then check if they are correct', () => {
-    const fruits = ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry']
-
-    cy.contains('#fruits-paragraph', "You haven't selected any fruit yet.")
-    .should('be.visible')
-    
-    cy.get('#fruit').select(fruits)
-    
-    const lowerCaseFruits = fruits.map(fruit => fruit.toLowerCase()) 
-    
-    cy.contains('#fruits-paragraph', "You've selected the following fruits: " + lowerCaseFruits.join(', '))
-    .should('be.visible')
-  })
-
-  it.only('upload a file and check the correct file name is displayed', () => {
-    cy.get('#file-upload').should('be.visible')
-
-    cy.get('#file-upload').selectFile('./cypress/fixtures/example.json')
-    cy.contains(
-      '#file', 
-      'The following file has been selected for upload: example.json'
-    ).should('be.visible')
-  })
 })
